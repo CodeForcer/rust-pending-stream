cd722082e4fc862fb02b9109fb0255c203ed7ec8
codeforcer <nathancworsley@gmail.com>
2021-05-31 20:34:16 +0100

    benchmarks

---
+++ benchmarks/pending-stream.ts
@@ -0,0 +1,28 @@
+import { WebSocketProvider } from '@ethersproject/providers'
+import { providers } from 'ethers'
+
+const getWsProvider = (): WebSocketProvider =>
+  new providers.WebSocketProvider('ws://localhost:8546')
+
+async function main() {
+  const provider = getWsProvider()
+
+  let count = 0
+  provider.on('pending', async (txHash: string) => {
+    const tx = await provider.getTransaction(txHash)
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    if (!tx) {
+      // console.error(`Abort: "tx null" hash=${txHash}`)
+      return
+    }
+    count++
+    console.log(count, txHash)
+    return
+  })
+}
+  
+main().catch((error) => {
+  console.error('[EXIT WITH ERROR]', error)
+  process.exit(1)
+})
+  
